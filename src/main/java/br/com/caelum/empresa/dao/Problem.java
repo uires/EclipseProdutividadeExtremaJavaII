a7f5b294ee0c0e916d7c7d4037fd920f099022ea
uires deivide <uiresdeivide1@gmail.com>
2017-03-08 16:34:56 -0300

    Aula 7- Usando Ctrl + 3 , Problems para identificar erros;

---
+++ src/main/java/br/com/caelum/empresa/dao/Problem.java
@@ -0,0 +1,16 @@
+package br.com.caelum.empresa.dao;
+
+public class Problem {
+
+    private int umAtributo;
+
+    private void umMetodo() {
+        int numero = 12;
+
+        String nome = "Código";
+        nome = nome.concat(" muito bagunçado!");
+
+        int b = 0;
+        b++;
+    }
+}
