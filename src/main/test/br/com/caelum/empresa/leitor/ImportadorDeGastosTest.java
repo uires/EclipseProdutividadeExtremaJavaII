716f1f80fe14c49b66202ff1e3eb16a4902bcf09
uires deivide <uiresdeivide1@gmail.com>
2017-03-08 17:00:35 -0300

    Aula 9- Testes, JUnit 4

---
+++ src/main/test/br/com/caelum/empresa/leitor/ImportadorDeGastosTest.java
@@ -0,0 +1,15 @@
+package br.com.caelum.empresa.leitor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ImportadorDeGastosTest {
+
+	@Test
+	public void deveRetornarListaVaziaSeOArquivoForVazio() {
+		
+	}
+	
+	
+}
