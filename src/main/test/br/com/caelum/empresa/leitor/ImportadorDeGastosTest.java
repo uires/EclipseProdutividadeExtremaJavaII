3370f68ae5593d30b875c8ca8c1d4d99ff82c4c9
uires deivide <uiresdeivide1@gmail.com>
2017-03-10 14:17:40 -0300

    Aula 8- Source Folder e teste

--- src/main/test/br/com/caelum/empresa/leitor/ImportadorDeGastosTest.java
+++ src/main/test/br/com/caelum/empresa/leitor/ImportadorDeGastosTest.java
@@ -2,12 +2,28 @@ package br.com.caelum.empresa.leitor;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 import org.junit.Test;
+
+import br.com.caelum.empresa.dao.Gasto;
 
 public class ImportadorDeGastosTest {
 
 	@Test
-	public void deveRetornarListaVaziaSeOArquivoForVazio() {
+	public void deveRetornarListaVaziaSeOArquivoForVazio() throws ParseException {
+		String conteudo = "CARTAOx1012011000010000123jbjasbd jbjbbb                22071983\r\n";
+		ByteArrayInputStream input = new ByteArrayInputStream(conteudo.getBytes());
+		
+		ImportadorDeGastos gastos = new ImportadorDeGastos();
+		Collection<Gasto> lista = new ArrayList<Gasto>();
+		
+		assertTrue("A lista não está vazia",lista.isEmpty());
+		
 		
 	}
 	
